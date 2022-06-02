69b8f3b2619fff9fd2e53203ebc5366fa814cd87
brigitte.nikoll <roadclosed24@gmail.com>
2022-06-02 17:41:35 -0500

    Adding styles

--- src/components/listaTareas.js
+++ src/components/listaTareas.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import tareas from "../utils/tareas";
 
 const listaTareas = () => {
+  const botonIncompleta = (
+    <button className="btn btn-primary mr-2">Marcar incompleta</button>
+  );
+
+  const botonCompleta = (
+    <button className="btn btn-primary mr-2">Marcar completada</button>
+  );
+
+  const botonEditar = <button className="btn btn-warning">Editar</button>;
+  const botonEliminar = <button className="btn btn-danger">Eliminar</button>;
+
   return (
-    <div>listaTareas</div>
-  )
-}
+    <div className="row">
+      {tareas.map((tarea) => (
+        <div className="col-4 mb-4">
+          <div className="card" key={tarea.id}>
+            <div className="card-body">
+              <h5 className="card-title">{tarea.titulo}</h5>
+              <p className="card-text">
+                {tarea.competado ? "Tarea completada" : "Tarea incompleta"}
+              </p>
+              {tarea.completado ? botonIncompleta : botonCompleta}
+              {tarea.completado ? botonEliminar : botonEditar}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default listaTareas;
--- src/pages/principal.js
+++ src/pages/principal.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import Formulario from '../components/formulario'
-import ListaTareas from '../components/listaTareas'
+import React from "react";
+import Formulario from "../components/formulario";
+import ListaTareas from "../components/listaTareas";
+/* import tareas from "../utils/tareas" */
 
 const Principal = () => {
   return (
-<>
+    <>
       <div className="container">
         <h1 className="text-center mt-5 mb-5">Lista de tareas</h1>
-        <Formulario/>
-        <ListaTareas/>
+        <Formulario />
+        <ListaTareas />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Principal;
